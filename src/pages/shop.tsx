f15ff7f98c19bd8d30b9cfedaa1cf778013f1bd2
hieupt36555 <hieuptph36555@fpt.edu.vn>
2024-07-31 23:01:16 +0700

    commit 7

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -17,7 +17,7 @@ const ShopList: React.FC = () => {
     const getAllProduct = async () => {
         try {
           // setLoading(true);
-          const response = await axios.get("/products",
+          const response = await axios.get("https://xuong-react-server-2.onrender.com//products",
             {
               params: {
                 page: page,
